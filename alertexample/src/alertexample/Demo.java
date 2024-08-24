29b6066833c5ac488a5cd414f178b1aa6f8ebcd4
TINCY <tincystc@gmail.com>
2024-08-24 09:40:43 +0530

    Initialfiles

---
+++ alertexample/src/alertexample/Demo.java
@@ -0,0 +1,16 @@
+package alertexample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\selenium automation\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
